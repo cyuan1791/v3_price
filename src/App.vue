493b48d4210a6a33294477afb1fdab2f6bd939b3
Mark Yuan <mark@webasone.com>
2025-01-19 07:47:43 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import PricingComponent from './components/PricingComponent.vue'
+let postData = JSON.parse(atob(window.asoneData));  
+console.log(postData);
+
 </script>
 
 <template>
